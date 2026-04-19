f9ce51bac5bca248d6263aedbf1ac5ae3353503c
Alejandro Soriano <hola@alejandrosoriano.xyz>
2026-04-19 10:43:27 +0100

    Cover remember OFFSET/ROLL helpers in TC_config

--- tests/OrienterExpress/TC_config.rb
+++ tests/OrienterExpress/TC_config.rb
@@ -10,8 +10,10 @@ module ASM_Extensions
         language context_menu
         rotation_mode pivot_custom smooth_groups
         roll_step offset_step default_roll default_offset
+        remember_offset remember_roll
         dark_mode debug_mode
         oevertex_offset oecenter_offset oeaxisscale_offset oeuscale_offset oesurface_offset oeflow_offset
+        oevertex_roll oecenter_roll oeaxisscale_roll oeuscale_roll oesurface_roll oeflow_roll
       ].freeze
 
       # Keys sent by the frontend's currentSettings() — debug_mode is excluded intentionally
@@ -30,10 +32,22 @@ module ASM_Extensions
       end
 
       def test_default_config_boolean_values
-        bool_keys = %i[context_menu dark_mode debug_mode]
+        bool_keys = %i[context_menu dark_mode debug_mode remember_offset remember_roll smooth_groups]
         bool_keys.each do |key|
           val = DEFAULT_CONFIG[key]
           assert [true, false].include?(val), "#{key} should be boolean, got #{val.class}"
+        end
+      end
+
+      def test_default_remember_flags_are_true
+        assert_equal true, DEFAULT_CONFIG[:remember_offset]
+        assert_equal true, DEFAULT_CONFIG[:remember_roll]
+      end
+
+      def test_default_per_tool_offset_and_roll_are_nil
+        %i[oevertex oecenter oeaxisscale oeuscale oesurface oeflow].each do |tool|
+          assert_nil DEFAULT_CONFIG["#{tool}_offset".to_sym], "#{tool}_offset should default to nil"
+          assert_nil DEFAULT_CONFIG["#{tool}_roll".to_sym],   "#{tool}_roll should default to nil"
         end
       end
 
@@ -118,10 +132,68 @@ module ASM_Extensions
         end
       end
 
+      # --- load_offset_str / default_offset_str ---
+
+      def test_default_offset_str_matches_load_with_nil_key
+        CONFIG[:default_offset] = "3cm"
+        assert_equal OrienterExpress.load_offset_str(nil),
+                     OrienterExpress.default_offset_str
+      end
+
+      def test_load_offset_str_falls_back_to_default_when_per_tool_nil
+        CONFIG[:default_offset]   = "2cm"
+        CONFIG[:oevertex_offset]  = nil
+        assert_equal OrienterExpress.default_offset_str,
+                     OrienterExpress.load_offset_str(:oevertex_offset)
+      end
+
+      def test_load_offset_str_prefers_stored_value_over_default
+        CONFIG[:default_offset]  = "0cm"
+        CONFIG[:oevertex_offset] = "5cm"
+        refute_equal OrienterExpress.default_offset_str,
+                     OrienterExpress.load_offset_str(:oevertex_offset)
+      end
+
+      # --- load_last_roll_deg / set_last_roll ---
+
+      def test_load_last_roll_deg_falls_back_to_default_roll
+        CONFIG[:oevertex_roll] = nil
+        CONFIG[:default_roll]  = 12
+        assert_equal 12.0, OrienterExpress.load_last_roll_deg(:oevertex)
+      end
+
+      def test_load_last_roll_deg_uses_stored_config_value
+        CONFIG[:oevertex_roll] = 45
+        assert_equal 45.0, OrienterExpress.load_last_roll_deg(:oevertex)
+      end
+
+      def test_load_last_roll_deg_cache_takes_precedence_over_config
+        OrienterExpress.last_roll_store[:oevertex] = 90.0
+        CONFIG[:oevertex_roll] = 45
+        assert_equal 90.0, OrienterExpress.load_last_roll_deg(:oevertex)
+      end
+
+      def test_set_last_roll_persists_when_remember_roll_true
+        CONFIG[:remember_roll] = true
+        CONFIG[:oevertex_roll] = nil
+        OrienterExpress.set_last_roll(:oevertex, 60.0)
+        assert_equal 60.0, OrienterExpress.last_roll_store[:oevertex]
+        assert_equal 60.0, CONFIG[:oevertex_roll]
+      end
+
+      def test_set_last_roll_skips_persist_when_remember_roll_false
+        CONFIG[:remember_roll] = false
+        CONFIG[:oevertex_roll] = nil
+        OrienterExpress.set_last_roll(:oevertex, 60.0)
+        assert_equal 60.0, OrienterExpress.last_roll_store[:oevertex]
+        assert_nil CONFIG[:oevertex_roll]
+      end
+
       private
 
       def setup
         @config_backup = File.exist?(CONFIG_FILE) ? File.read(CONFIG_FILE) : nil
+        OrienterExpress.last_roll_store.clear
       end
 
       def teardown
@@ -131,6 +203,7 @@ module ASM_Extensions
         else
           FileUtils.rm_f(CONFIG_FILE)
         end
+        OrienterExpress.last_roll_store.clear
       end
 
       def load_config
